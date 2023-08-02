ce5025577c0af0823fcd075818b4462b76dadbfe
Laith-Vlad <laithxfg13@gmail.com>
2023-08-02 23:42:09 +0000

    back up sockets

--- src/sockets/backend/hub.js
+++ src/sockets/backend/hub.js
@@ -8,5 +8,23 @@ const ioServer = require('socket.io')(port);
 
 
 ioServer.on('connection', (socket) =>{ // connection event emitted automatically by Sockt io
-    console.log('Welcome, your socket id:', socket.id); })
-    
+    console.log('Welcome, your socket id:', socket.id)
+    socket.on('pickHandyman',pickedHandyman)
+
+    function pickedHandyman(payload) {
+        // emiting to the handyman
+    ioServer.emit('client-recived', payload)    
+
+    console.log(`client requast is successfull with payload: `,payload.handyman)
+
+     }
+
+     socket.on('busyHandyMan',busy)
+     function busy (payload){
+        ioServer.emit('handymanIsBusy',payload)
+
+        console.log('handyman is busy')
+        
+     }
+
+    ; })
