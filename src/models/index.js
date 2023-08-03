ef2f41e6b2a24a0b45846acb6659ef4f372ed05e
Basharirani23 <super_boshbosh@hotmail.com>
2023-08-03 13:36:12 +0300

    sign up done

--- src/models/index.js
+++ src/models/index.js
@@ -1,24 +1,33 @@
-require('dotenv').config();
-const userModel=require('../auth/authModels/users');
-const handymenModel=require('../auth/authModels/handymen');
-const companiesModel=require('../auth/authModels/companies');
-const employeesModel=require('./employees');
-const expertiesModel=require('./experties');
-const { Sequelize, DataTypes } = require('sequelize');
-const DATABASE_URL = process.env.DBURL || 'sqlite:memory;';
-const sequelize= new Sequelize(DATABASE_URL);
-const handymen=handymenModel(sequelize,DataTypes);
-const users=userModel(sequelize,DataTypes);
-const experties=expertiesModel(sequelize,DataTypes);
-const companies=companiesModel(sequelize,DataTypes);
-const employees=employeesModel(sequelize,DataTypes);
-experties.belongsToMany(handymen,{through:'experties_handymen'});
-handymen.belongsToMany(experties,{through:'experties_handymen'});
+require("dotenv").config();
+
+const userModel = require("../auth/authModels/users");
+const handymenModel = require("../auth/authModels/handymen");
+const companiesModel = require("../auth/authModels/companies");
+const employeesModel = require("./employees");
+const expertiesModel = require("./experties");
+const { Sequelize, DataTypes } = require("sequelize");
+const secret = process.env.SECRET;
+const DATABASE_URL = process.env.DBURL || "sqlite:memory;";
+const sequelize = new Sequelize(DATABASE_URL);
+
+const handymen = handymenModel(sequelize, DataTypes);
+const users = userModel(sequelize, DataTypes, secret);
+const experties = expertiesModel(sequelize, DataTypes);
+const companies = companiesModel(sequelize, DataTypes);
+const employees = employeesModel(sequelize, DataTypes);
+
+experties.belongsToMany(handymen, { through: "experties_handymen" });
+handymen.belongsToMany(experties, { through: "experties_handymen" });
 companies.hasMany(employees);
 employees.hasOne(companies);
-module.exports={
-    db:sequelize,
+
+const models = {
+    db: sequelize,
     users,
     handymen,
     experties,
+    companies,
+    employees,
 };
+
+module.exports = models;
--- src/server.js
+++ src/server.js
@@ -5,94 +5,116 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-const paypal = require('paypal-rest-sdk');paypal.configure({
-    'mode': 'sandbox', //sandbox or live
-    'client_id': 'AcyIAQLKbRXdOcNfWFPE1wpV5bs7ILj4RlL8kNemuObX2SZC-9EJx0qB8sKskSc2AcuFy_Nc_TpaH_lc',
-    'client_secret': 'EB6WLpykxU28mFPAup2B6rlGuPw9QltUpYlUWCeq7N7NHVUyZHieToyEH7grrRXtucnEytUrYySLn2SU'
-  });
-const pageNotFound = require("./middlewares/404");
-const serverError = require("./middlewares/500");
-const logger = require('./middlewares/logger')
+const paypal = require("paypal-rest-sdk");
+const router = express.Router();
 
-
-app.use(logger);
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
-app.post('/pay', (req, res) => {
-    const create_payment_json = {
-      "intent": "sale",
-      "payer": {
-          "payment_method": "paypal"
-      },
-      "redirect_urls": {
-          "return_url": "http://localhost:3000/success",
-          "cancel_url": "http://localhost:3000/cancel"
-      },
-      "transactions": [{
-          "item_list": {
-              "items": [{
-                  "name": "Red Sox Hat",
-                  "sku": "001",
-                  "price": "50.00",
-                  "currency": "USD",
-                  "quantity": 1
-              }]
-          },
-          "amount": {
-              "currency": "USD",
-              "total": "50.00"
-          },
-          "description": "Hat for the best team ever"
-      }]
-  };
-    app.get('/success', (req, res) => {
-    const payerId = req.query.PayerID;
-    const paymentId = req.query.paymentId;
-  
-    const execute_payment_json = {
-      "payer_id": payerId,
-      "transactions": [{
-          "amount": {
-              "currency": "USD",
-              "total": "25.00"
-          }
-      }]
-    };
-  
-    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-      if (error) {
-          console.log(error.response);
-          throw error;
-      } else {
-          console.log(JSON.stringify(payment));
-          res.send('Success');
-      }
-  });
-  });    paypal.payment.create(create_payment_json, function (error, payment) {
-        if (error) {
-            throw error;
-        } else {
-            for(let i = 0;i < payment.links.length;i++){
-              if(payment.links[i].rel === 'approval_url'){
-                res.redirect(payment.links[i].href);
-              }
-            }
-        }
-      });
-      
-      });  app.get('/cancel', (req, res) => res.send('Cancelled'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+paypal.configure({
+    mode: "sandbox", //sandbox or live
+    client_id:
+        "AcyIAQLKbRXdOcNfWFPE1wpV5bs7ILj4RlL8kNemuObX2SZC-9EJx0qB8sKskSc2AcuFy_Nc_TpaH_lc",
+    client_secret:
+        "EB6WLpykxU28mFPAup2B6rlGuPw9QltUpYlUWCeq7N7NHVUyZHieToyEH7grrRXtucnEytUrYySLn2SU",
+});
+const pageNotFound = require("./middlewares/404");
+const serverError = require("./middlewares/500");
+const logger = require("./middlewares/logger");
 
+const companySignUp = require("../src/routes/auth/signup"); // Update the path accordingly
+app.use(companySignUp);
+
+// const signUp = require("../src/routes/auth/signup");
+router.post("/CompanySignup", companySignUp);
+
+app.use(logger);
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.post("/pay", (req, res) => {
+    const create_payment_json = {
+        intent: "sale",
+        payer: {
+            payment_method: "paypal",
+        },
+        redirect_urls: {
+            return_url: "http://localhost:3000/success",
+            cancel_url: "http://localhost:3000/cancel",
+        },
+        transactions: [
+            {
+                item_list: {
+                    items: [
+                        {
+                            name: "Red Sox Hat",
+                            sku: "001",
+                            price: "50.00",
+                            currency: "USD",
+                            quantity: 1,
+                        },
+                    ],
+                },
+                amount: {
+                    currency: "USD",
+                    total: "50.00",
+                },
+                description: "Hat for the best team ever",
+            },
+        ],
+    };
+    app.get("/success", (req, res) => {
+        const payerId = req.query.PayerID;
+        const paymentId = req.query.paymentId;
+
+        const execute_payment_json = {
+            payer_id: payerId,
+            transactions: [
+                {
+                    amount: {
+                        currency: "USD",
+                        total: "25.00",
+                    },
+                },
+            ],
+        };
+
+        paypal.payment.execute(
+            paymentId,
+            execute_payment_json,
+            function (error, payment) {
+                if (error) {
+                    console.log(error.response);
+                    throw error;
+                } else {
+                    console.log(JSON.stringify(payment));
+                    res.send("Success");
+                }
+            }
+        );
+    });
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            for (let i = 0; i < payment.links.length; i++) {
+                if (payment.links[i].rel === "approval_url") {
+                    res.redirect(payment.links[i].href);
+                }
+            }
+        }
+    });
+});
+app.get("/cancel", (req, res) => res.send("Cancelled"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("*", pageNotFound);
 app.use(serverError);
 
 function start(port) {
-  app.listen(port, () => console.log(`up and running on port: ${port}`));
+    app.listen(port, () => console.log(`up and running on port: ${port}`));
 }
 
 module.exports = {
-  app,
-  start,
+    app,
+    start,
 };
