bdefce97982382511ce3db57a832bfd0dc644efd
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-08-11 16:17:54 +0000

    private-events-part3

--- src/sockets/backend/hub.js
+++ src/sockets/backend/hub.js
@@ -1,27 +1,148 @@
-// socketModule.js
+'use strict';
+
+
+
 const socketIO = require('socket.io');
 
-module.exports = (server) => {
-    const ioServer = socketIO(server);
 
-    ioServer.on('connection', (socket) => {
-        // connection event emitted automatically by Socket.io
-        console.log('Welcome, your socket id:', socket.id);
+// const allUsers= {'rama123': 'jdfhsdufj',
+// 'handyman1':'241356'
+// }
+module.exports= (server)=> {
+   const ioServer = socketIO(server);
 
-        socket.on('pickHandyman', pickedHandyman);
-        function pickedHandyman(payload) {
-            // Emitting to the handyman
-            ioServer.emit('client-recived', payload);
+ioServer.on('connection', async (socket) =>{ // connection event emitted automatically by Sockt io
+   
+    console.log('Welcome, your socket id:', socket.id)
 
-            console.log(`Client request is successful with payload:`, payload.handyman);
+    
+
+    socket.on('pickHandyman',pickedHandyman)
+
+    function pickedHandyman(payload) {
+        // emiting to the handyman
+    ioServer.emit('client-recived', payload)
+
+    console.log(`client requast is successfull with payload:` ,payload.handyman)
+
+     }
+
+     socket.on('busyHandyMan',busy)
+     function busy (payload){
+        ioServer.emit('handymanIsBusy',payload)
+
+        console.log('handyman is busy')
+        
+     }
+     socket.on('schedualeAndpayment', handlePaymentAndScheduale) // handman
+     function handlePaymentAndScheduale(payload)
+     {
+        // distance calculations here
+        payload.status = true;  // here it should be true if the payment logic worked false if payment logic failed and the transaction failed
+        ioServer.emit('transaction',payload) // every where
+        
+     }
+     socket.on('choiceToContinue',choiceToContinueLate)
+     function choiceToContinueLate(payload) {
+      
+      ioServer.emit('choiceToContinue',payload)
+
+      if(payload.client.choice==false){
+         payload.payback=true;
+         console.log('Client chose not to continue returning the amount of ', payload.payment,'to client named',payload.client.name)
+         ioServer.emit('returendYouMoney',payload)
+         
+         
+        
+     }    
+      
+
+     }
+     socket.on('returnStageOne',returningPayment)
+     function returningPayment (payload) {
+      console.log('returning amount :::',payload.payment,'to client',payload.client.name)
+     }
+
+     socket.on('arrived',arrivedOrLate)
+     function arrivedOrLate(payload) {
+      if (payload.onTime===true) {
+         console.log('arrived on time ready for work',payload)
+         ioServer.emit('arrived',payload)
+      } else {
+         console.log('arrived late and wating for client response ') // add logic for canceling if the user wants and to get money back to the user
+         // ioServer.emit('arrived',payload)
+         console.log('arrived on time ready for work',payload)
+         ioServer.emit('late',payload)
+      }
+
+     }
+
+     // from the handyman 
+     socket.on('costestimate',estimate) // log the price and emmit to the client and handy man if accepted
+     function estimate(payload) {
+      console.log('product costs ',payload.costEstimate.price)
+      
+      ioServer.emit('costestimate',payload) // from hub to all
+     }
+     // service acepted
+     socket.on('paidTotal',startWorking)
+     function startWorking(payload) {
+      console.log('amount paid',payload.costEstimate)
+       ioServer.emit('startWorking',payload)
+     }
+    
+     // service declined 
+     socket.on('serviceRejected',nextClient)
+     function nextClient(){
+      console.log('rejected')
+      ioServer.emit('serviceRejected')
+     }
+     /// third stage charge 
+   
+     socket.on('ontimeorless',finishedOnTime)
+     function finishedOnTime(payload) {
+      let oneHoursFixer = payload.costEstimate.expectedWorkTime-payload.deffrance/payload.costEstimate.expectedWorkTime //  to make any time we used for testing as an hour to calc hourly rate
+      payload.costEstimate.hourlyPayment = payload.costEstimate.hourlyRate*oneHoursFixer;
+      console.log('::::::::test ', payload.costEstimate.hourlyPayment)
+      console.log('third stage payment ',payload.costEstimate.hourlyPayment)
+      ioServer.emit('lastPayment',payload)
+
+     }
+     socket.on('moreCharge',finishedlate)
+     function finishedlate(payload) {
+      let oneHoursFixer = (payload.costEstimate.expectedWorkTime+payload.deffrance)/payload.costEstimate.expectedWorkTime //  to make any time we used for testing as an hour to calc hourly rate
+      payload.costEstimate.hourlyPayment = payload.costEstimate.hourlyRate*oneHoursFixer;
+      console.log('::::::::test ', payload.costEstimate.hourlyPayment)
+      console.log('third stage payment ',payload.costEstimate.hourlyPayment)
+      ioServer.emit('lastPayment',payload)
+
+     }
+     
+     socket.on('paidrdStage',confirmedpayment) //from user
+     function confirmedpayment (payload){
+        
+        console.log ('paied for this operation',payload.costEstimate)
+        ioServer.emit('paidrdStage',payload) // to handyman
+        socket.on('reviewOfHandyman',sendingServer)// sending the review to server
+        
+        function sendingServer(payload) {
+         console.log("the operator",payload.handyman.name," got the rating of", payload.handyman.review, 'for this operation')
+         
+         // logic to send to the server here
         }
+       
+        
 
-        socket.on('busyHandyMan', busy);
-        function busy(payload) {
-            ioServer.emit('handymanIsBusy', payload);
-            console.log('Handyman is busy');
-        }
-    });
+        } 
+        socket.on('reviewOfclient',sendingCleintToServer)
+        function sendingCleintToServer(payload) { 
+         console.log('the client',payload.client.name,"got the rating of ",payload.client.review,' for this interaciton' )
+         // logic to send to the server here 
+        
+      }
+      
 
-    return ioServer;
-};
+    ; })
+    
+   }
+
