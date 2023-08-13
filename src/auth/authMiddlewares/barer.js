de546225ba0b9fc53467fd69ce6de713ba7e6724
Armada6199 <moh.abdin27@gmail.com>
2023-08-14 00:14:37 +0300

    test

--- src/auth/authMiddlewares/barer.js
+++ src/auth/authMiddlewares/barer.js
@@ -6,7 +6,7 @@ module.exports =(model)=>{
   return async (req, res, next) => {
     try {
       if (!req.headers.authorization) { authError() }
-      const token = req.headers.authorization.split(' ').pop();
+      const token = req.headers.authorization.split(' ').pop(); 
       const validUser = await model.authenticateToken(model,token);
       req.user = validUser;
       req.token = validUser.token;
