39328009b80f43e0c917d3656d376656476afa98
Laith-Vlad <laithxfg13@gmail.com>
2023-08-13 16:19:44 +0000

    finsih

--- src/models/index.js
+++ src/models/index.js
@@ -8,8 +8,25 @@ const inboxParticipants=require('./messaging/inbox_participants');
 const inbox=require('./messaging/inbox');
 const review=require('./review');
 const { Sequelize, DataTypes } = require('sequelize');
-const DATABASE_URL = process.env.DBURL || 'sqlite:memory;';
-const sequelize= new Sequelize(DATABASE_URL);
+
+
+
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DBURL;
+
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+} : {};
+
+
+
+const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
+
+
 const handymenModel=handymen(sequelize,DataTypes);
 const userModel=user(sequelize,DataTypes);
 const expertyModel=experty(sequelize,DataTypes);
