c30e9c6678f4b8025502197c1260be13c27ca954
Armada6199 <moh.abdin27@gmail.com>
2023-08-14 11:22:56 +0300

    .

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -3,15 +3,10 @@ const router =require('express').Router();
 const barerAuth=require('../auth/authMiddlewares/barer');
 const {messageModel,inboxModel,inboxParticipantsModel}=require('../models');
 // Should be able to fetch all the conversataions for a specific user
-router.get('/inboxes',async(req,res)=>{
-  const all=await inboxModel.findAll();
-  res.send(all);
-})
 router.get('/conversatations/:userId',async(req,res)=>{
-  const role=req.query.role;
 try {
   const userId=req.params.userId;
-  const allConvos=await inboxModel.findConversations(userId,role);
+  const allConvos=await inboxParticipantsModel.findConversations(userId);
   res.send(allConvos);
 } catch (e) {
   res.send(e);
@@ -21,35 +16,28 @@ try {
 router.get('/messages/:conversationId',async(req,res,next)=>{
   const conversationId=req.params.conversationId;
   try {
-    const allMessages=await messageModel.findAll({where:{InboxId:conversationId}});
+    const allMessages=await messageModel.findAll({where:{inboxId:conversationId});
     res.send(allMessages);  
   } catch (e) {
     res.send(e);
   };
 });
 // Should be able to send a message with the two users id and create a new inbox
-router.post('/messages/:user1Id/:user2Id',async(req,res,next)=>{
+router.post('/message/:user1Id/:user2Id',async(req,res,next)=>{
   const {user1Id,user2Id}=req.params;
   const {inboxId,userId,content,sentAt}=req.body;
   if(inboxId){
     try {
-      const sentMessage=await messageModel.create({messageContent:content,InboxId:inboxId,userId:userId,sentAt:sentAt});
-     console.log('exitsts')
+      const sentMessage=await messageModel.create({messageContent:content,inboxId:inboxId,userId:userId,sentAt:sentAt});
       res.send(sentMessage);
      } catch (e) {
-     console.log(e);
+     
    }
   }else {
-    try {
-      const newInbox=await inboxModel.create({lastMessage:content,UserId:user1Id,HandymanId:user2Id});
-      console.log(newInbox)
-      const newMessage=await messageModel.create({messageContent:content,InboxId:newInbox.id,UserId:userId,sentAt:sentAt});
-    res.send(newInbox);
-    } catch (e) {
-      console.log(e)
-      res.send(e);
-    }
-    
+    const newInbox=await inboxModel.create({lastMessage:content});
+    const newInbox_participants=await inboxParticipantsModel.create({inboxId:newInbox.id,user1Id:user1Id,user2Id:user2Id});
+    const newMessage=messageModel.create({messageContent:content,inboxId:inboxId,userId:userId,sentAt:sentAt});
+    res.send(newMessage);
   };
 });
 module.exports=router;
